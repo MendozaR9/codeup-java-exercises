b10405af10e8d5f33837a6ac8432fa1f09487643
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-04-07 10:53:14 -0500

    finish table of power working on number to letter grade

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 ////        int i =5;
 ////        while (i  <= 15){
 ////            System.out.print(i+" ");
@@ -31,16 +33,38 @@ public class ControlFlowExercises {
 ////            squaredof2 = (int) Math.pow(squaredof2,2);
 ////        }while (squaredof2 < 1000000);
 
-        for (int frezzbuzz =1; frezzbuzz <=100; frezzbuzz++){
-            System.out.print("["+frezzbuzz+"]");
-            if (frezzbuzz % 3 == 0 && frezzbuzz % 5 ==0) {
-                System.out.print(" FrizzBuzz");
-            }else if (frezzbuzz % 3 ==0){
-                System.out.print(" Frizz");
-            }else if (frezzbuzz % 5==0){
-                System.out.print(" Buzz");
+//        for (int frezzbuzz =1; frezzbuzz <=100; frezzbuzz++){
+//            System.out.print("["+frezzbuzz+"]");
+//            if (frezzbuzz % 3 == 0 && frezzbuzz % 5 ==0) {
+//                System.out.print(" FrizzBuzz");
+//            }else if (frezzbuzz % 3 ==0){
+//                System.out.print(" Frizz");
+//            }else if (frezzbuzz % 5==0){
+//                System.out.print(" Buzz");
+//            }
+//            System.out.println();
+//        }
+
+        boolean continued;
+        do {
+            System.out.println("What number would you like to go up to? ");
+            String stopingValueString = scanner.next();
+            System.out.println("Here is your table!");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            int stopingValue = Integer.parseInt(stopingValueString);
+            for (int i = 1; i <= stopingValue; i++) {
+                int squared = (int) Math.pow(i, 2);
+                int cubed = (int) Math.pow(i, 3);
+                System.out.print(i + "      |");
+                System.out.print(" " + squared + "       |");
+                System.out.println(" " + cubed + "   ");
             }
-            System.out.println();
-        }
+            System.out.println("Do you want to Continue? (Y/N)");
+            String userConinues = scanner.next();
+            continued = userConinues.equalsIgnoreCase("y");
+        }while (continued);
+        System.out.println("ok bye 👋");
+
     }
 }
