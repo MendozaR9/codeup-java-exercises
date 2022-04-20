a2fe567bc2d6fbba511d4b0bd9e4937112159b81
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-04-20 11:12:09 -0500

    Finish collections

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,5 +1,7 @@
 package grades;
 
+import util.Input;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,18 +13,13 @@ public class GradesApplication {
         HashMap<String, Student> students = new HashMap<>();
         students.put("MendozaR", new Student("Ryan"));
         Student student1 = students.get("MendozaR");
-
-//        student1.addGrade(100);
-//        student1.addGrade(100);
-//        student1.addGrade(90);
-
         giveGrades(student1, 100, 100, 90);
-        System.out.println(student1.getGradeAverage());
+//        System.out.println(student1.getGradeAverage());
 
         students.put("Tasty-RiceCake",new Student("Joe"));
         Student student2 = students.get("Tasty-RiceCake");
         giveGrades(student2, 80, 75, 95);
-        System.out.println(student2.getGradeAverage());
+//        System.out.println(student2.getGradeAverage());
 
         students.put("bobbyBoi", new Student("Bob"));
         Student student3 = students.get("bobbyBoi");
@@ -31,9 +28,48 @@ public class GradesApplication {
         students.put("kyle", new Student("Kyle"));
         Student student4 = students.get("kyle");
         giveGrades(student4, 50, 60, 70);
-        System.out.println(students.keySet());
-        System.out.println(getKey(students, student4));
+//        System.out.println(students.keySet());
+//        System.out.println(getKey(students, student4));
+
+        Input input = new Input();
+        boolean seeAnotherStudent = true;
+        while (seeAnotherStudent) {
+            System.out.println("\nWellcome!\n" +
+                    "Here are the Github usernames of our students");
+            System.out.println("|" + getKey(students, student1) + "| " +
+                    "|" + getKey(students, student2) + "| " +
+                    "|" + getKey(students, student3) + "| " +
+                    "|" + getKey(students, student4) + "| \n" +
+                    "What Student would you like to see more information on?");
+            System.out.print("> ");
+            String username = input.getString();
+            if (username.equals(getKey(students, student1))) {
+                System.out.println(studentInfo(students, student1));
+            }
+            else if (username.equals(getKey(students, student2))) {
+                System.out.println(studentInfo(students, student2));
+            }
+            else if (username.equals(getKey(students, student3))) {
+                System.out.println(studentInfo(students, student3));
+            }
+            else if (username.equals(getKey(students, student4))) {
+                System.out.println(studentInfo(students, student4));
+            }else {
+                System.out.println("Sorry, no student found with the Github" +
+                        " username of "+username);
+            }
+
+
+
+            System.out.println("\nWould you like to see another student?");
+             if (!new Input().yesNo()){
+                 seeAnotherStudent = false;
+             }
+        }
+        System.out.println("Goodbye, and have a wonderful day");
     }
+
+
     public static <K, V> K getKey(Map<K, V> map, V value)
     {
         for (Map.Entry<K, V> entry: map.entrySet())
@@ -44,6 +80,15 @@ public class GradesApplication {
         }
         return null;
     }
+
+
+public static String studentInfo(HashMap student, Student value){
+
+        return "Name: "+value.getNames()+
+                " - Github Username: "+ getKey(student,value)+"\n" +
+            "Current Average: "+value.getGradeAverage();
+}
+
     public static void giveGrades(Student student, int grade1,int grade2 , int grade3){
         student.addGrade(grade1);
         student.addGrade(grade2);
